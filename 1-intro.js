67f3c23bb532d6d232f9e042ab9c05830b8b7901
khushwantD <101801251+khushwantD@users.noreply.github.com>
2023-04-12 16:40:41 +0530

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const num = 20;
+
+if(num > 15) {
+    console.log('the amount is greater');
+}else{
+    console.log('the amount is lower');
+}
+
+console.log('my  first node project(not really)');
---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+// Modules - Enscapulated code ( only  share minimum)
+//CommonJS, every file is module (by default)
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavour')
+require('./7-mind-grenade');
+
+console.log(data);
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
